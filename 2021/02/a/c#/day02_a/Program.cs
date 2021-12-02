038611889db7b764d44d1aa4843c958dfce0efc5
Nicholas Judge <nsjudge@outlook.com>
2021-12-02 15:53:34 -0800

    AOC 2021, day 2 (c#)

---
+++ 2021/02/a/c#/day02_a/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(args.Length);
+if (args.Length != 1)
+{
+    throw new ArgumentException();
+}
+
+var lines = new List<string>();
+using (var reader = new StreamReader(args[0]))
+{
+    while (!reader.EndOfStream)
+    {
+        string? line = await reader.ReadLineAsync();
+        if (string.IsNullOrEmpty(line))
+        {
+            continue;
+        }
+
+        lines.Add(line);
+    }
+}
+
+int horizontal = 0;
+int depth = 0;
+var regex = new Regex(@"^(forward|down|up)\s+(\d+)\s*$", RegexOptions.Compiled);
+foreach (string line in lines)
+{
+    Match match = regex.Match(line);
+    if (!match.Success)
+    {
+        continue;
+    }
+
+    var delta = int.Parse(match.Groups[2].Value);
+    switch (match.Groups[1].Value)
+    {
+        case "forward":
+        horizontal += delta;
+        break;
+
+        case "down":
+        depth += delta;
+        break;
+
+        case "up":
+        depth -= delta;
+        break;
+    }
+}
+
+Console.WriteLine($"Horizontal: {horizontal}");
+Console.WriteLine($"Depth: {depth}");
+Console.WriteLine($"Combined: {horizontal * depth}");
