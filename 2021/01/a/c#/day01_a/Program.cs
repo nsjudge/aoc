bff47284afb3260e553d5bcd37d482ad1374a695
Nicholas Judge <nsjudge@outlook.com>
2021-12-01 22:07:41 -0800

    AOC 2021, day 1 (c#)

---
+++ 2021/01/a/c#/day01_a/Program.cs
@@ -0,0 +1,30 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException();
+}
+
+var readings = new List<int>();
+using (var reader = new StreamReader(args[0]))
+{
+    while (!reader.EndOfStream)
+    {
+        string? line = await reader.ReadLineAsync();
+        if (!int.TryParse(line, out int depth))
+        {
+            continue;
+        }
+
+        readings.Add(depth);
+    }
+}
+
+int deeper = 0;
+for (int index = 1; index < readings.Count; ++index)
+{
+    if (readings[index] > readings[index - 1])
+    {
+        ++deeper;
+    }
+}
+
+Console.WriteLine($"Count: {deeper}");
---
+++ 2021/01/b/c#/day01_b/Program.cs
@@ -0,0 +1,36 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException();
+}
+
+var readings = new List<int>();
+using (var reader = new StreamReader(args[0]))
+{
+    while (!reader.EndOfStream)
+    {
+        string? line = await reader.ReadLineAsync();
+        if (!int.TryParse(line, out int depth))
+        {
+            continue;
+        }
+
+        readings.Add(depth);
+    }
+}
+
+var windows = new List<int>();
+for (int index = 2; index < readings.Count; ++index)
+{
+    windows.Add(readings[index - 2] + readings[index - 1] + readings[index]);
+}
+
+int deeper = 0;
+for (int index = 1; index < windows.Count; ++index)
+{
+    if (windows[index] > windows[index - 1])
+    {
+        ++deeper;
+    }
+}
+
+Console.WriteLine($"Count: {deeper}");
