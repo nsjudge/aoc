0f2dabc3f4db8373a50d4e9524444b097cd0f5c2
Nicholas Judge <nsjudge@Nicks-Air.localdomain>
2021-12-03 23:20:13 -0800

    AOC 2021, day 3 (c#)

---
+++ 2021/03/a/c#/day03_a/Program.cs
@@ -0,0 +1,73 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException();
+}
+
+var lines = new List<string>();
+using (var reader = new StreamReader(args[0]))
+{
+    while (!reader.EndOfStream)
+    {
+        string? line = await reader.ReadLineAsync();
+        if (string.IsNullOrEmpty(line))
+        {
+            continue;
+        }
+    
+        lines.Add(line);
+    }
+}
+
+var counts = new List<bits>();
+foreach (string line in lines)
+{
+    int pos = 0;
+    foreach (char bit in line)
+    {
+        if ((bit != '0') && (bit != '1'))
+        {
+            continue;
+        }
+
+        if (counts.Count <= pos)
+        {
+            counts.Add(new bits());
+        }
+
+        bits count = counts[pos++];
+        if (bit == '0')
+        {
+            ++count.zeros;
+        }
+        else
+        {
+            ++count.ones;
+        }
+    }
+}
+
+int epsilon = 0;
+int gamma = 0;
+foreach (bits count in counts)
+{
+    epsilon <<= 1;
+    gamma <<= 1;
+    if (count.ones > count.zeros)
+    {
+        gamma |= 1;
+    }
+    else
+    {
+        epsilon |= 1;
+    }
+}
+
+Console.WriteLine($"gamma: {gamma}");
+Console.WriteLine($"epsilon: {epsilon}");
+Console.WriteLine($"power: {gamma * epsilon}");
+
+class bits
+{
+    public int zeros;
+    public int ones;
+}
---
+++ 2021/03/b/c#/day03_b/Program.cs
@@ -0,0 +1,92 @@
+﻿if (args.Length != 1)
+{
+    throw new ArgumentException();
+}
+
+var lines = new List<string>();
+using (var reader = new StreamReader(args[0]))
+{
+    while (!reader.EndOfStream)
+    {
+        string? line = await reader.ReadLineAsync();
+        if (string.IsNullOrEmpty(line))
+        {
+            continue;
+        }
+    
+        lines.Add(line);
+    }
+}
+
+List<string> candidates = lines;
+int index = 0;
+while (candidates.Count > 1)
+{
+    bits count = bits.count(candidates, index);
+    bool ones = count.ones >= count.zeros;
+    candidates = candidates.Where(l => ones ? l[index] == '1' : l[index] == '0').ToList();
+    ++index;
+}
+
+int oxygen = BinaryToInt(candidates.ElementAt(0));
+
+candidates = lines;
+index = 0;
+while (candidates.Count > 1)
+{
+    bits count = bits.count(candidates, index);
+    bool zeros = count.zeros <= count.ones;
+    candidates = candidates.Where(l => zeros ? l[index] == '0' : l[index] == '1').ToList();
+    ++index;
+}
+
+int co2 = BinaryToInt(candidates.ElementAt(0));
+
+Console.WriteLine($"oxygen: {oxygen}");
+Console.WriteLine($"co2: {co2}");
+Console.WriteLine($"life: {oxygen * co2}");
+
+int BinaryToInt(string binary)
+{
+    int val = 0;
+    foreach (char bit in binary)
+    {
+        if ((bit != '0') && (bit != '1'))
+        {
+            continue;
+        }
+
+        val <<= 1;
+        if (bit == '1')
+        {
+            val |= 1;
+        }
+    }
+
+    return val;
+}
+
+class bits
+{
+    public int zeros;
+    public int ones;
+
+    public static bits count(IEnumerable<string> candidates, int index)
+    {
+        var counts = new bits();
+        foreach (string test in candidates)
+        {
+            if (test[index] == '1')
+            {
+                ++counts.ones;
+            }
+            else
+            {
+                ++counts.zeros;
+            }
+        }
+
+        return counts;
+    }
+}
+
